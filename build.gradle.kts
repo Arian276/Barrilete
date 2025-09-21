4ef0222038a7049061255c5e149137aead41c11b
Arian276 <114463200+Arian276@users.noreply.github.com>
2025-09-21 04:52:17 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,11 @@
-// Archivo: app/build.gradle.kts
+// Archivo build.gradle.kts del proyecto raíz
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+id("com.android.application") version "8.4.2" apply false
+id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
-android {
-    namespace = "com.bc.tvappvlc"   // ⚡️ajustalo según tu packageName
-    compileSdk = 34
+tasks.register("clean", Delete::class) {
+delete(layout.buildDirectory)
+}
 
-    defaultConfig {
-        applicationId = "com.bc.tvappvlc"  // ⚡️ajustalo igual que tu packageName
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions
